e4b8def3093666398ddedfe76ac7bf17cba6d7d6
Alinny Ribeiro <alinnyrc6@gmail.com>
2023-04-20 22:22:03 -0300

    Concertando erro
    O beecrowd não aceitava o código como ele estava, ai eu finalmente consegui consertar

--- FormulaBhaskara.py
+++ FormulaBhaskara.py
@@ -1,16 +1,15 @@
-a,b,c = input().split(" ")
+#Fórmula de Bháskara
+v = input().split()
+a, b, c = v
 
-a=float(a)
-b=float(b)
-c=float(c)
+a = float(a)
+b = float(b)
+c = float(c)
 
-delta=(b**2)-(4*a*c)
-
-
-if delta<=0 or a==0:
-    print("Impossivel calcular")
+if a == 0.0  or (b ** 2 - 4 * a * c) < 0:
+    print('Impossivel calcular')
 else:
-    delta=delta**0.5
-    r1= ((-b)+delta)/2*a
-    r2= ((-b)-delta)/2*a
-    print("R1 = %.5f\nR2 = %.5f" %(r1,r2))
+    x1 = (- b + (b ** 2 - 4 * a * c) ** (1/2) )/(2 * a)
+    x2 = (- b - (b ** 2 - 4 * a * c) ** (1/2) )/(2 * a)
+    print('R1 = %.5f' %x1)
+    print('R2 = %.5f' %x2)
